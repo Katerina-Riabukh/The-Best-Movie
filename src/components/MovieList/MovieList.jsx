9ab46208b627c1767b934dbdd788b636c7a3f2a4
Katerina-Riabukh <katrysya7773@gmail.com>
2023-08-09 20:28:10 +0200

    +++

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,7 +1,6 @@
 import css from './MovieList.module.css'
 import { Link, useLocation } from "react-router-dom";
 
-
 const MovieList = ({ response, searchValue }) => {
     const location = useLocation()
 
@@ -14,7 +13,7 @@ const MovieList = ({ response, searchValue }) => {
 
                         return (<Link key={id} to={`/MovieInfo/${id}_${title}`} state={{ from: location }} className={css.movieItem}>
                             <li>
-                                <img src={poster_path ? `https://image.tmdb.org/t/p/w500${poster_path}` : '/src/default/images/cinema-4153289_640.jpg'} alt={title} />
+                                <img src={poster_path ? `https://image.tmdb.org/t/p/w500${poster_path}` : '/src/default/images/cinema.jpg'} alt={title} />
                                 <p className={css.title}>{title}</p>
                             </li>
                         </Link>)
